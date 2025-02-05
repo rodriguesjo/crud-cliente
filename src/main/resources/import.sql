eccc09e8023a1e0c5b188964dac6195c6da2f1a8
rodriguesjo <rodriguesjo8@outlook.com>
2025-02-04 21:39:23 -0300

    Criado classe client e o Seeding de clients

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrick Jane', '22233311155', 5500.00, '1974-06-12', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Teresa Lisbon', '11122233344', 7800.00, '1975-08-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Grace Van Pelt', '99988877766', 4500.00, '1980-12-05', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Wayne Rigsby', '12312345656', 4800.00, '1979-11-10', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Kimball Cho', '19873519545', 6150.00, '1977-01-04', null);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Thomas McAllister', '07516814578', 7000.00, '1968-09-15', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Dennis Abbott', '06814915567', 11000.00, '1970-10-10', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Simon Baker', '22331105445', 7500.00, '1974-08-16', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Robin Tunney', '15435467912', 8600.00, '1982-07-27', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Amanda Righetti', '12345678912', 6850.00, '1987-04-09', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Owain Yeoman', '14725836912', 7000.00, '1985-03-21', null);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tim Kang', '14725836910', 9750.00, '1981-01-01', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Xander Berkeley', '91373145678', 9030.00, '1960-10-12', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rockmond Dunbar', '15975345613', 10100.00, '1975-02-20', 2);
