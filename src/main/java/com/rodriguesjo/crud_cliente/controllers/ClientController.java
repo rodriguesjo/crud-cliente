8bcf86ec52a296a31ea953bf3031695509b921d8
rodriguesjo <rodriguesjo8@outlook.com>
2025-02-08 11:28:32 -0300

    PUT client

--- src/main/java/com/rodriguesjo/crud_cliente/controllers/ClientController.java
+++ src/main/java/com/rodriguesjo/crud_cliente/controllers/ClientController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -37,5 +36,11 @@ public class ClientController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
